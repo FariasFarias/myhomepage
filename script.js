0de2b13c939c6704bcf682188a05d2862d674790
Breno Farias <brenopalheta1999@gmail.com>
2023-02-22 19:48:58 -0300

    interação

--- script.js
+++ script.js
@@ -14,3 +14,34 @@ function mudarImg(direction)
         img.setAttribute('src', `images/${((imgNum - 1) + 3) % 3}.jpg`);  
     }
 }
+
+function submitName()
+{
+    txt = document.querySelector("#text");
+    inputName = document.querySelector("#input").value; 
+    chest = document.querySelector("#chest");
+
+    if (inputName != '')
+    {
+        txt.innerHTML = `It's dangerous to go alone, ${inputName.trim().charAt(0).toUpperCase() + inputName.trim().slice(1)}!<br>Take this!`;
+        txt.style.display = 'block';
+        chest.style.display = 'block';
+
+        document.querySelector("#question").style.display = "none";
+        document.querySelector("#input").style.display = "none";
+        document.querySelector("#btn").style.display = "none"
+    }
+    else
+    {
+        alert("Tell me your name!")
+    }
+}
+
+function openChest()
+{
+    document.querySelector("#chest").setAttribute("src", '../images/cat.png');
+    txt = document.querySelector("#text");
+
+    txt.innerHTML = "Congratulation! you got a cute cat!";
+    txt.style.color = "Green";
+}
