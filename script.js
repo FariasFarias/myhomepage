5ba3fd95e1ed3e76ea8145137eeaae7c69153e59
Breno Farias <brenopalheta1999@gmail.com>
2023-02-22 14:47:40 -0300

    adicionando novas imagens
    - add imagens
    - ajuste de estilo

---
+++ script.js
@@ -0,0 +1,16 @@
+function mudarImg(direction)
+{
+    img = document.querySelector("#img");
+    path = img.getAttribute('src');
+
+    imgNum = Number.parseInt(path[8]);
+    
+    if(direction == 'next')
+    {
+        img.setAttribute('src',`/images/${(imgNum + 1) % 3}.jpg`);
+    }
+    else if(direction == 'prev')
+    {
+        img.setAttribute('src', `/images/${((imgNum - 1) + 3) % 3}.jpg`);  
+    }
+}
