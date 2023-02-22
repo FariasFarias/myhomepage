881893c4f70d3e73057a81fdfd5edb381721e66b
Breno Farias <brenopalheta1999@gmail.com>
2023-02-22 15:14:16 -0300

    reorganizar de imgs

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ function mudarImg(direction)
     img = document.querySelector("#img");
     path = img.getAttribute('src');
 
-    imgNum = Number.parseInt(path[8]);
+    imgNum = Number.parseInt(path[7]);
     
     if(direction == 'next')
     {
-        img.setAttribute('src',`/images/${(imgNum + 1) % 3}.jpg`);
+        img.setAttribute('src',`images/${(imgNum + 1) % 3}.jpg`);
     }
     else if(direction == 'prev')
     {
-        img.setAttribute('src', `/images/${((imgNum - 1) + 3) % 3}.jpg`);  
+        img.setAttribute('src', `images/${((imgNum - 1) + 3) % 3}.jpg`);  
     }
 }
